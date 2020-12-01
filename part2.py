d8e89fd4719d317f62095a7e0221230575fe76a6
GregoryKaraev <74561902+GregoryKaraev@users.noreply.github.com>
2020-12-01 13:25:48 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -10,6 +10,8 @@ number in the formatted sentence that's already there.
 '''
 
 #code starts here
-number = input("Enter a number: ")
-print("Ten times your number is", 10 * number)
-print("Ninety-nine more than your number is", 99 + number)
+number = int(input("Enter a number: "))
+print("Ten times your number is " + str(10 * number) + ".")
+print("Ninety-nine more than your number is " + str(99 + number) + ".")
+
+#I removed commas, added plus signs, and made number an integer, so that it didn't just repeat the number a bunch of times.
